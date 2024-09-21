60952683cf8d6668c03c3a68280465ddbf01b763
sabarinath4 <sabarinathr04@gmail.com>
2024-09-21 17:08:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-Implementation"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "End-To-End-Data-Science-Project-With-ML-Flow"
+AUTHOR_USER_NAME = "sabarinath4"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "sabarinathr04@gmail.com"
 
 
 setuptools.setup(
